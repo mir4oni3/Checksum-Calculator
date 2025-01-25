5e73f273f46758fcba8200c3db924bfc1b78f49c
Miroslav Stoyanovski <m.stoyanovski3@gmail.com>
2025-01-25 11:33:02 +0200

    UserActions implementation

--- imp/inc/UserActions.hpp
+++ imp/inc/UserActions.hpp
@@ -3,12 +3,13 @@
 #include "../inc/InputFacade.hpp"
 #include "../inc/ChecksumCalculator.hpp"
 #include "../inc/File.hpp"
+#include "../inc/FileParser.hpp"
 
 #include <memory>
 
 class UserActions {
 public:
-    void start(const InputFacade& input);
-    void viewChecksums(const std::shared_ptr<File>& target, const std::shared_ptr<ChecksumCalculator>& calc);
-    void compareChecksums(const std::shared_ptr<File>& target, const std::shared_ptr<ChecksumCalculator>& calc, const std::unordered_map<std::string, std::string>& checksums);
+    void start(const InputFacade& input) const;
+    void viewChecksums(const std::shared_ptr<File>& target, const std::shared_ptr<ChecksumCalculator>& calc, const std::shared_ptr<FileParser>& parser) const;
+    void compareChecksums(const std::shared_ptr<File>& target, const std::shared_ptr<ChecksumCalculator>& calc, std::unordered_map<std::string, std::string>& checksums) const;
 };
--- imp/lib/UserActions.cpp
+++ imp/lib/UserActions.cpp
@@ -2,10 +2,11 @@
 #include "../inc/CalculatorFactory.hpp"
 #include "../inc/FileParserFactory.hpp"
 #include "../inc/FileSystemBuilderFactory.hpp"
+#include "../inc/FileIterator.hpp"
 
 #include <memory>
 
-void UserActions::start(const InputFacade& input) {
+void UserActions::start(const InputFacade& input) const {
     std::shared_ptr<FileSystemBuilder> builder = FileSystemBuilderFactory::getBuilder(input.getTraverse());
     if (input.getBuildChecksums()) {
         builder->setAlgorithm(input.getAlgorithm());
@@ -17,7 +18,7 @@ void UserActions::start(const InputFacade& input) {
     
 
     if (input.getChecksums() == "") {
-        viewChecksums(target, calc);
+        viewChecksums(target, calc, parser);
         return;
     }
 
@@ -27,6 +28,48 @@ void UserActions::start(const InputFacade& input) {
         throw std::runtime_error("FileActions::viewChecksums - Error opening file");
     }
     std::unordered_map<std::string, std::string> checksums = parser->parseFiles(checksumFile);
-
     compareChecksums(target, calc, checksums);
+}
+
+void UserActions::viewChecksums(const std::shared_ptr<File>& target, const std::shared_ptr<ChecksumCalculator>& calc, const std::shared_ptr<FileParser>& parser) const {
+    if (!parser) {	
+        throw std::invalid_argument("UserActions::viewChecksums - nullptr parser passed");
+    }
+    parser->exportFile(target, std::cout, calc);
+}
+
+void UserActions::compareChecksums(const std::shared_ptr<File>& target, const std::shared_ptr<ChecksumCalculator>& calc, std::unordered_map<std::string, std::string>& checksums) const {
+    if (!target || !calc) {
+        throw std::invalid_argument("UserActions::compareChecksums - nullptr arg(s) passed");
+    }
+    
+    FileIterator it(target);
+    while (it.hasMore()) {
+        std::shared_ptr<RegularFile> regFile = std::dynamic_pointer_cast<RegularFile>(it.next());
+        if (!regFile) {
+            continue;
+        }
+
+        std::string currentPath = regFile->getPath();
+        std::string checksum = regFile->getChecksum(calc);
+
+        if (checksums.find(currentPath) == checksums.end()) {
+            std::cout << currentPath << ": NEW\n";
+            continue;
+        }
+        if (checksum != checksums.at(currentPath)) {
+            std::cout << currentPath << ": MODIFIED\n";
+            checksums.erase(currentPath);
+            continue;
+        }
+        if (checksum == checksums.at(currentPath)) {
+            std::cout << currentPath << ": OK\n";
+            checksums.erase(currentPath);
+            continue;
+        }
+    }
+
+    for (const auto& [path, checksum] : checksums) {
+        std::cout << path << ": REMOVED\n";
+    }
 }
