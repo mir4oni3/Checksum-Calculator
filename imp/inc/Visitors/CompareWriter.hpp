b166d48751642837d12a3b232aae1e1f66906d4f
mir4oni3 <m.stoyanovski3@gmail.com>
2025-01-29 13:50:54 +0200

    CompareWriter implementation

---
+++ imp/inc/Visitors/CompareWriter.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Visitors/HashStreamWriter.hpp"
+
+class CompareWriter : public HashStreamWriter {
+protected:
+    std::unordered_map<std::string, std::string>& checksums;
+public:
+    CompareWriter(std::ostream& os, const std::shared_ptr<ChecksumCalculator>& calc, std::unordered_map<std::string, std::string>& checksums);
+    void visitRegularFile(const RegularFile& file) const override;
+    void finalizeExport() const override;
+    void addObserver(const std::shared_ptr<Observer>& observer) override;
+};
---
+++ imp/lib/Visitors/CompareWriter.cpp
@@ -0,0 +1,35 @@
+#include "Visitors/CompareWriter.hpp"
+
+CompareWriter::CompareWriter(std::ostream& os, const std::shared_ptr<ChecksumCalculator>& calc, std::unordered_map<std::string, std::string>& checksums) : HashStreamWriter(os, calc), checksums(checksums) {}
+
+void CompareWriter::visitRegularFile(const RegularFile& regFile) const {
+    VisitorWriter::visitRegularFile(regFile); //notify observers
+
+    std::string currentPath = regFile.getPath();
+    std::string checksum = regFile.getChecksum(calc);
+
+    if (checksums.find(currentPath) == checksums.end()) {
+        tempStream << currentPath << ": NEW\n";
+    }
+    else if (checksum != checksums.at(currentPath)) {
+        tempStream << currentPath << ": MODIFIED\n";
+        checksums.erase(currentPath);
+    }
+    else if (checksum == checksums.at(currentPath)) {
+        tempStream << currentPath << ": OK\n";
+        checksums.erase(currentPath);
+    }
+}
+
+void CompareWriter::finalizeExport() const {
+    HashStreamWriter::finalizeExport();
+
+    for (const auto& [path, checksum] : checksums) {
+        tempStream << path << ": REMOVED\n";
+    }
+}
+
+void CompareWriter::addObserver(const std::shared_ptr<Observer>& observer) {
+    VisitorWriter::addObserver(observer);
+    calc->addObserver(observer);
+}
