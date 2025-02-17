2a55943a4404489c1fadbd5cdca7ae277ad660c9
mir4oni3 <m.stoyanovski3@gmail.com>
2025-02-17 16:06:37 +0200

    Changed FileIterator UTs due to new behavior

--- tst/Files/TestFileIterator.cpp
+++ tst/Files/TestFileIterator.cpp
@@ -15,11 +15,12 @@ TEST_CASE("TestFileIteratorNextRegularFile", "[fileIterator]") {
     FileIterator fileIterator(file);
     const File* nextFile;
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     REQUIRE(nextFile == &file);
 
-    REQUIRE_NOTHROW(nextFile = &fileIterator.next());
-    REQUIRE(nextFile == nullptr);
+    REQUIRE(!fileIterator.hasNext());
+    REQUIRE_THROWS_AS(fileIterator.next(), std::invalid_argument);
 }
 
 //these files with random content were created with the following bash command:
@@ -39,6 +40,7 @@ TEST_CASE("TestFileIteratorNextDirectory", "[fileIterator]") {
     //first file is the directory itself
     std::filesystem::path sampleTestDirectory = std::filesystem::absolute(HelperTestFunctions::getResourceFile("sampleTestDirectory"));
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = sampleTestDirectory.string();
     REQUIRE(nextFile != nullptr);
@@ -46,16 +48,19 @@ TEST_CASE("TestFileIteratorNextDirectory", "[fileIterator]") {
 
     //next files are the gibberish files
     
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = (sampleTestDirectory / "gibberishFile1.txt").string();
     REQUIRE(nextFile != nullptr);
     REQUIRE(nextFile->getPath() == expected);
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = (sampleTestDirectory / "gibberishFile2.txt").string();
     REQUIRE(nextFile != nullptr);
     REQUIRE(nextFile->getPath() == expected);
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = (sampleTestDirectory / "gibberishFile3.txt").string();
     REQUIRE(nextFile != nullptr);
@@ -64,16 +69,19 @@ TEST_CASE("TestFileIteratorNextDirectory", "[fileIterator]") {
     //next is the sampleDir1 and its subfiles
     std::filesystem::path sampleDir1 = sampleTestDirectory / "sampleDir1";
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = sampleDir1.string();
     REQUIRE(nextFile != nullptr);
     REQUIRE(nextFile->getPath() == expected);
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = (sampleDir1 / "gibberishFile4.txt").string();
     REQUIRE(nextFile != nullptr);
     REQUIRE(nextFile->getPath() == expected);
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = (sampleDir1 / "gibberishFile5.txt").string();
     REQUIRE(nextFile != nullptr);
@@ -82,23 +90,25 @@ TEST_CASE("TestFileIteratorNextDirectory", "[fileIterator]") {
     //next is the sampleDir2 and its subfiles
     std::filesystem::path sampleDir2 = sampleTestDirectory / "sampleDir2";
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = sampleDir2.string();
     REQUIRE(nextFile != nullptr);
     REQUIRE(nextFile->getPath() == expected);
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = (sampleDir2 / "gibberishFile6.txt").string();
     REQUIRE(nextFile != nullptr);
     REQUIRE(nextFile->getPath() == expected);
 
+    REQUIRE(fileIterator.hasNext());
     REQUIRE_NOTHROW(nextFile = &fileIterator.next());
     expected = (sampleDir2 / "gibberishFile7.txt").string();
     REQUIRE(nextFile != nullptr);
     REQUIRE(nextFile->getPath() == expected);
 
     //we traversed the files, now nullptr should be returned
-    REQUIRE_NOTHROW(nextFile = &fileIterator.next());
-    REQUIRE(nextFile == nullptr);
-
+    REQUIRE(!fileIterator.hasNext());
+    REQUIRE_THROWS_AS(fileIterator.next(), std::invalid_argument);
 }
